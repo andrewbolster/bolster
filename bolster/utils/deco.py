0f638cbec9d3fd3ecd9054c68b7f3eee434535b3
Andrew Bolster <me@andrewbolster.info>
2021-05-18 14:35:53 +0100

    Timed Decorator function

---
+++ bolster/utils/deco.py
@@ -0,0 +1,19 @@
+import logging
+import time
+
+from functools import wraps
+
+
+def timed(func):
+    """This decorator prints the execution time for the decorated function."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        logging.info(f"Launching {func.__name__}")
+        result = func(*args, **kwargs)
+        end = time.time()
+        logging.info(f"{func.__name__} ran in {round(end - start, 2)}s")
+        return result
+
+    return wrapper
