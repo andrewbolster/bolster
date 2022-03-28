16201b935f6dc532507bb6d1b76b788c09be2d06
Andrew Bolster <me@andrewbolster.info>
2022-03-28 12:27:22 +0100

    Adding in HI HPI data wrangling toolkit

---
+++ tests/test_nihpi.py
@@ -0,0 +1,29 @@
+import unittest
+
+import requests_cache
+
+from bolster.data_sources import ni_house_price_index as hpi
+
+
+class MyTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        requests_cache.install_cache(expire_after=3600, allowable_methods=("GET",))
+
+    def test_source_size(self):
+        dfs = hpi.pull_source()
+        self.assertEqual(len(dfs), 36)  # Source has changed size, should be 36 long!
+
+    def test_output_size(self):
+        dfs = hpi.build()
+        self.assertEqual(len(dfs), 33)  # Final has changed size, should be 33
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        path = requests_cache.get_cache().db_path
+        requests_cache.uninstall_cache()
+        path.unlink()
+
+
+if __name__ == "__main__":
+    unittest.main()
