65a419d55d850d211ad3d6499ea003acbe8e020b
Andrew Bolster <andrew.bolster@gmail.com>
2024-02-21 17:43:36 +0000

    the road to 0.3

--- noxfile.py
+++ noxfile.py
@@ -19,7 +19,7 @@ except ImportError:
     raise SystemExit(dedent(message))
 
 package = "bolster"
-python_versions = ["3.10", "3.9", "3.8"]
+python_versions = ["3.11", "3.10", "3.9", "3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
@@ -213,7 +213,7 @@ def docs_build(session: Session) -> None:
     session.run("sphinx-build", *args)
 
 
-@session(python="3.9")
+@session(python="3.10")
 def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
