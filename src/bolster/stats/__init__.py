ef77c82aef322d4714559ecf2f341fb4eb2655db
Andrew Bolster <bolster@blackduck.com>
2024-11-15 09:47:58 +0000

    drop_totals

--- src/bolster/stats/__init__.py
+++ src/bolster/stats/__init__.py
@@ -45,6 +45,52 @@ def add_totals(
 
     return df
 
+def drop_totals(
+    df: pd.DataFrame,
+    column_total: AnyStr = "total",
+    row_total: AnyStr = "total",
+    inplace=True,
+) -> pd.DataFrame:
+    """
+    Remove Row and Column totals from a dataframe (in place)
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame from which to remove totals.
+    column_total : AnyStr, optional
+        The name of the column total, by default "total".
+    row_total : AnyStr, optional
+        The name of the row total, by default "total".
+    inplace : bool, optional
+        Whether to modify the DataFrame in place, by default True.
+
+    Returns
+    -------
+    pd.DataFrame
+        The DataFrame with totals removed.
+    
+    Examples
+    --------
+    >>> df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'total': [5, 7, 9]})
+    >>> df.loc['total'] = [6, 15, 21]
+    >>> drop_totals(df)
+       A  B
+    0  1  4
+    1  2  5
+    2  3  6
+    """
+    if not inplace:
+        df = df.copy(deep=True)
+    
+    if column_total in df.columns:
+        df = df.drop(columns=[column_total])
+    
+    if row_total in df.index:
+        df = df.drop(index=[row_total])
+    
+    return df
+
 
 def fix_datetime_tz_columns(df: pd.DataFrame, inplace=True) -> pd.DataFrame:
     """
@@ -69,3 +115,39 @@ def fix_datetime_tz_columns(df: pd.DataFrame, inplace=True) -> pd.DataFrame:
     for date_column in date_columns:
         df[date_column] = df[date_column].dt.tz_localize(None)
     return df
+
+def top_n(df: pd.DataFrame, 
+          n: int,
+          others:AnyStr="others") -> pd.DataFrame:
+    """
+    Truncate the DataFrame to the top 'n' rows, summing all subsequent rows into an 'others' row.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to truncate.
+    n : int
+        The number of top rows to keep.
+
+    Returns
+    -------
+    pd.DataFrame
+        The truncated DataFrame with an 'others' row.
+    
+    Examples
+    --------
+    >>> df = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [5, 4, 3, 2, 1]})
+    >>> top_n(df, 3)
+           A  B
+    0      1  5
+    1      2  4
+    2      3  3
+    others 9  3
+    """
+    if n >= len(df):
+        return df
+
+    top_df = df.iloc[:n]
+    others_df = df.iloc[n:].sum(numeric_only=True)
+    others_df.name = others
+    return pd.concat([top_df, others_df.to_frame().T])
